5b6891cd272416a66da129a5d8e5437e15a4e759
Piotr Korbas <piotrkorbas@gmail.com>
2022-12-23 14:15:49 +0100

    add fancy image and fancy underline

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const object = document.querySelector('.kask-image'); // pobierz obiekt HTML
-const  target1 = document.querySelector('.description-button');
-let currentRotation = 0;  // zmienna przechowująca obecny kąt obrotu
+const kask = document.querySelector('.kask-image'); 
+let currentRotation = 0;  
 
-document.addEventListener('mousemove', (event) => {  // nasłuchuj na ruch myszy
-  const x = event.clientX;  // pobierz pozycję x kursora
-  const y = event.clientY;  // pobierz pozycję y kursora
 
-  const targetRotation = Math.atan2(-y + object.offsetTop, -x + object.offsetLeft) * 180 / Math.PI;  // oblicz kąt docelowy
-  const limitedRotation = Math.max(Math.min(targetRotation, 8), 0);  // ogranicz kąt do zakresu od 0 do 35 stopni
-  const rotationDiff = limitedRotation - currentRotation;  // oblicz różnicę między obecnym a docelowym kątem
+// rotacja kasku
+document.addEventListener('mousemove', (event) => {  
+  const x = event.clientX;  
+  const y = event.clientY;  
+
+  const targetRotation = Math.atan2(-y + kask.offsetTop, -x + kask.offsetLeft) * 180 / Math.PI;  
+  const limitedRotation = Math.max(Math.min(targetRotation, 8), 0);  
+  const rotationDiff = limitedRotation - currentRotation;  
   currentRotation += rotationDiff * 0.1;  
-  object.style.transform = `rotate(${currentRotation}deg)`;  
+  kask.style.transform = `rotate(${currentRotation}deg)`;  
 });
+
