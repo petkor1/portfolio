a54f74acb755ce78996a89825940acf046ad0925
Piotr Korbas <piotrkorbas@gmail.com>
2022-12-27 11:28:39 +0100

    add sidebar

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const kask = document.querySelector('.kask-image'); 
-const menuLink = [...document.querySelectorAll('.menu-link')];
-const sound = document.getElementById('sound');
+import getElement from "./utils/getElement.js";
+
+const kask = document.querySelector('.kask-image');
+const closeBtn = document.querySelector('.close-sidebar-btn');
+const openBtn = document.querySelector('.open-sidebar-btn')
+const sidebar = document.querySelector('.nav-sidebar');
+
 let currentRotation = 0;  
 
 
 // rotacja kasku darth vadera
-document.addEventListener('mousemove', (event) => {  
+if(kask){
+  document.addEventListener('mousemove', (event) => {  
   const x = event.clientX;  
   const y = event.clientY;  
 
@@ -15,10 +20,15 @@ document.addEventListener('mousemove', (event) => {
   currentRotation += rotationDiff * 0.1;  
   kask.style.transform = `rotate(${currentRotation}deg)`;  
 });
+}
 
-// efekt dźwiękowy po najechaniu na link z menu
-// menuLink.forEach(item => {
-//     item.addEventListener('mouseenter', function(){
-//         sound.play();
-//     })
-// })
+if(sidebar){
+  closeBtn.addEventListener('click', function(){
+  sidebar.classList.remove('show-sidebar')
+});
+openBtn.addEventListener('click', function(){
+  sidebar.classList.add('show-sidebar')
+})
+}
+
+
